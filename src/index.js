78061c6a9f2916de1a3e28976d2729c79da3d451
Zach Strout <zachstrout@gmail.com>
2022-06-30 23:23:49 -0500

    Make a PWA
    There were a bunch of things like icons and sw that I needed to add.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './Styles/index.css';
 import App from './App';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -9,4 +11,4 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
+serviceWorkerRegistration.register()
